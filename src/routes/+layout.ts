69d943ceded142ad8dea8460e55711e3186547bc
Kenny <101839505+khui0@users.noreply.github.com>
2025-02-05 00:53:29 -0500

    fix: vercel

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -6,6 +6,3 @@ import "@fontsource-variable/rubik";
 import { dev } from "$app/environment";
 import { inject } from "@vercel/analytics";
 inject({ mode: dev ? "development" : "production" });
-
-import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-injectSpeedInsights();
