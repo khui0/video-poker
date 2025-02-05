d78e1ef6eddb50ec54793bad81098a63050017e3
Kenny <101839505+khui0@users.noreply.github.com>
2025-02-05 00:52:05 -0500

    feat: vercel

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,3 +2,10 @@
 import "@fontsource-variable/inter";
 // Supports weights 300-900
 import "@fontsource-variable/rubik";
+
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
+inject({ mode: dev ? "development" : "production" });
+
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+injectSpeedInsights();
